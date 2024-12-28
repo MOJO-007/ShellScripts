b5e0c19b09b48249eda452c7c2a895d4e6a2e541
MOJO-007 <abhisatish75@gmail.com>
2024-12-28 09:50:21 +0530

    More Scripts added

---
+++ scripts/basic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+##this is a comment
+
+ABHI=abhi
+
+echo "This is $ABHI"
+
+#Wont print the variable value
+echo 'THIS IS $ABHI'
+
+
+echo "`cal`"
---
+++ scripts/varia.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+A=10
+B=20
+HOSTNAME=$(hostname)
+BRO=50
+##Wrong variable conventions, No special characters, [UnderScroe is allowed]
+1value = 30
+False@var = False
+Hyphen_a=WrongValue
+
+echo "$A"
+echo "$B"
+echo "$HOSTNAME"
+echo "$BRO"
+echo "1value"
+echo "$False@var"
+echo "Hyphen-a"
+
